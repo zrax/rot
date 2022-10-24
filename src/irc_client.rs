5194ae4ff4c5d9425340c79b46364c2eb210033b
Michael Hansen <zrax0111@gmail.com>
2022-10-24 14:01:00 -0700

    Add basic IRC connectivity.

--- src/irc_client.rs
+++ src/irc_client.rs
@@ -4,6 +4,7 @@ use crate::line_parse::{ParsedLine, parse_line};
 use std::time::Duration;
 use tokio::sync::mpsc;
 use tokio::net::TcpStream;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 pub struct IrcClient {
     db: RotDb,
@@ -41,27 +42,108 @@ impl IrcClient {
     }
 
     pub async fn run(&mut self) {
-        self.connect().await;
-
         let mut save_timer = tokio::time::interval(Duration::from_secs(15 * 60));
         save_timer.tick().await;    // The first tick comes immediately
 
+        let mut sock = match self.connect(false).await {
+            Some(sock) => sock,
+            None => return,
+        };
+
         loop {
+            let mut chunk = Vec::<u8>::new();
+            let mut buf = [0; 1024];
+
             tokio::select! {
+                result = sock.read(&mut buf) => {
+                    match result {
+                        Ok(0) => {
+                            eprintln!("Server closed the connection");
+                            sock = match self.connect(true).await {
+                                Some(sock) => sock,
+                                None => return,
+                            };
+                        }
+                        Ok(n) => {
+                            chunk.extend(&buf[0..n]);
+                            chunk = self.process_lines(&chunk);
+                        }
+                        Err(err) => {
+                            eprintln!("Failed to read from server: {}", err);
+                            sock = match self.connect(true).await {
+                                Some(sock) => sock,
+                                None => return,
+                            };
+                        }
+                    }
+                },
                 _ = save_timer.tick() => self.db.sync(),
                 _ = self.shutdown_recv.recv() => break,
             }
         }
 
-        println!("Would disconnect cleanly");
+        // Still connected, so try to perform a graceful departure
+        let _ = sock.write_all(b"QUIT :--rot!\r\n").await;
     }
 
-    async fn connect(&self) {
-        // TODO
-        println!("Would connect to {}", self.remote_addr);
-        println!("Would set IDENT to {0} . . {0}", self.nick);
-        for chan in &self.channels {
-            println!("Would join #{}", chan);
+    fn process_lines(&mut self, mut chunk: &[u8]) -> Vec<u8> {
+        while let Some(pos) = chunk.iter().position(|c| *c == b'\n') {
+            let line_str = String::from_utf8_lossy(&chunk[0..pos]).trim_end().to_owned();
+            chunk = &chunk[pos + 1..];
+
+            println!("Debug: [{}]", line_str);
         }
+        // Return the remainder for the next call
+        chunk.to_owned()
+    }
+
+    async fn reconnect_delay(&mut self) -> bool {
+        eprintln!("Retrying in 60 sec...");
+        tokio::select! {
+            _ = tokio::time::sleep(Duration::from_secs(60)) => true,
+            _ = self.shutdown_recv.recv() => false,
+        }
+    }
+
+    async fn connect(&mut self, initial_delay: bool) -> Option<TcpStream> {
+        if initial_delay && !self.reconnect_delay().await {
+            return None;
+        }
+
+        let mut sock = loop {
+            let connect_fut = TcpStream::connect(&self.remote_addr);
+            match tokio::time::timeout(Duration::from_secs(60), connect_fut).await {
+                Ok(Ok(sock)) => break sock,
+                Ok(Err(err)) => {
+                    eprintln!("Failed to connect to {}: {}", self.remote_addr, err);
+                }
+                Err(_) => eprintln!("Connection timed out"),
+            };
+
+            if !self.reconnect_delay().await {
+                return None;
+            }
+        };
+
+        let peer_name = match sock.peer_addr() {
+            Ok(addr) => addr.to_string(),
+            Err(_) => "<unknown>".to_string(),
+        };
+        println!("Connected to {}", peer_name);
+
+        // Minimal identification necessary to satisfy the IRC server
+        let _ = sock.write_all(
+                    format!("NICK {0}\r\n\
+                             USER {0} . . :{0}\r\n", self.nick).as_bytes()
+                ).await;
+
+        // Join the requested IRC channel(s)
+        for chan in &self.channels {
+            let _ = sock.write_all(format!("JOIN #{}\r\n", chan).as_bytes()).await;
+        }
+
+        // If we lost the connection during the writes above, we'll catch it
+        // when we try to read from the socket in the main loop.
+        Some(sock)
     }
 }
